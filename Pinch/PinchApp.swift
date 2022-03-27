be6a4c3974f7a465e48fe3aaaa31772b0cbbaf14
Light2288 <davide.aliti@gmail.com>
2022-03-27 22:50:56 +0200

    Initial Commit

---
+++ Pinch/PinchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchApp.swift
+//  Pinch
+//
+//  Created by Davide Aliti on 27/03/22.
+//
+
+import SwiftUI
+
+@main
+struct PinchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
