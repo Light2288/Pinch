23ea7fa9112f31499ab397d1157f6040f225ba08
Light2288 <davide.aliti@gmail.com>
2022-04-03 22:47:07 +0200

    add icons add content add double click to zoom add drag movement to move zoomed picture add info panel

---
+++ Pinch/Screen/ContentView.swift
@@ -0,0 +1,86 @@
+//
+//  ContentView.swift
+//  Pinch
+//
+//  Created by Davide Aliti on 27/03/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    // MARK: - Property
+    @State private var isAnimating: Bool = false
+    @State private var imageScale: CGFloat = 1
+    @State private var imageOffset: CGSize = .zero
+    
+    // MARK: - Function
+    func resetImageState() {
+        return withAnimation(.spring()) {
+            imageScale = 1
+            imageOffset = .zero
+        }
+    }
+    
+    // MARK: - Content
+    var body: some View {
+        NavigationView {
+            ZStack {
+                Color.clear
+                // MARK: - Page Image
+                Image("magazine-front-cover")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .cornerRadius(10)
+                    .padding()
+                    .shadow(color: .black.opacity(0.2), radius: 12, x: 2, y: 2)
+                    .opacity(isAnimating ? 1 : 0)
+                    .offset(x: imageOffset.width, y: imageOffset.height)
+                    .animation(.linear(duration: 1), value: isAnimating)
+                    .scaleEffect(imageScale)
+                    .onTapGesture(count: 2) {
+                        if imageScale == 1 {
+                            withAnimation(.linear(duration: 0.2)) {
+                                imageScale = 5
+                            }
+                        } else {
+                            resetImageState()
+                        }
+                    }
+                    .gesture(
+                        DragGesture()
+                            .onChanged({ value in
+                                withAnimation(.linear(duration: 1)) {
+                                    imageOffset = value.translation
+                                }
+                            })
+                            .onEnded({ _ in
+                                if imageScale <= 1 {
+                                    resetImageState()
+                                }
+                            })
+                    )
+            }
+            .navigationTitle("Pinch & Zoom")
+            .navigationBarTitleDisplayMode(.inline)
+            .onAppear(perform: {
+              isAnimating = true
+            })
+            // MARK: - Info Panel
+            .overlay(
+                InfoPanelView(scale: imageScale, offset: imageOffset)
+                    .padding(.horizontal)
+                    .padding(.top, 30),
+                alignment: .top
+            )
+            
+        }
+        .navigationViewStyle(.stack)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+        
+    }
+}
